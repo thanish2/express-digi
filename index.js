ee96b6d42ef78a1b81e75706fc44769d9961e492
Thanish P M <thanipm20@gmail.com>
2025-09-29 22:49:55 +0530

    add dotenv for environment variable management and update .gitignore

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //.use acts as a middleware and is run at every request
 //express.json is used to convert json object recieved from client to javascript object..so req.body now has js object
@@ -34,26 +35,26 @@ app.get("/teas/:id", (req, res) => {
 });
 
 //update tea
-app.put("/teas/:id",(req,res)=>{
+app.put("/teas/:id", (req, res) => {
   const tea = teaData.find((t) => t.id === parseInt(req.params.id));
   if (!tea) {
     return res.status(404).send("Tea not found");
   }
-  const {name,price}=req.body
-  tea.name=name
-  tea.price=price
-  res.status(200).send(tea)
-})
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+  res.status(200).send(tea);
+});
 
 //delete tea
-app.delete("/teas/:id",(req,res)=>{
-  const index=teaData.findIndex(t=>t.id===parseInt(req.params.id))
-  if(index==-1){
-    return res.status(404).send("Tea not found")
+app.delete("/teas/:id", (req, res) => {
+  const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
+  if (index == -1) {
+    return res.status(404).send("Tea not found");
   }
-  teaData.splice(index,1)
-  res.status(204).send("Deleted")
-})
+  teaData.splice(index, 1);
+  res.status(204).send("Deleted");
+});
 
 app.listen(port, () => {
   //if it succesfully listens to port then function will be executed
